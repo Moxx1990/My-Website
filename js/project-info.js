4363e808f54a0c54eb16d393720c3ce3efd022be
moxx1990 <schmidt-max@gmx.de>
2026-07-08 10:51:18 +0200

    Added final links

--- js/project-info.js
+++ js/project-info.js
@@ -16,21 +16,21 @@ const projectsData = {
         number: "01",
         info: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
         languages: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-        link: 'https://join-frontend.web.app/',
+        link: 'https://join.schmidtmaximilian.com',
         github: 'https://github.com/Moxx1990/join'
     },
     'Bow of the Dead': {
         number: "02",
         info: 'Jump, run and shoot games based on object-oriented approach. Help the Samurai to find sake and arrows to fight against the zombies.',
         languages: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://bow-of-the-dead.web.app/',
+        link: 'https://bowofthedead.schmidtmaximilian.com',
         github: 'https://github.com/Moxx1990/2D-Sidescroller---Bow-of-the-Dead'
     },
     'Pokédex': {
         number: "03",
         info: 'Pokedex is a web application that provides information about various Pokémon. It is built with HTML, CSS, and JavaScript. It uses the PokéAPI to fetch data. Users can search for Pokémon by name.',
         languages: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://pokedex-maxschmidt.web.app/',
+        link: 'https://pokedex.schmidtmaximilian.com',
         github: 'https://github.com/Moxx1990/Pokedex'
     }
 };
